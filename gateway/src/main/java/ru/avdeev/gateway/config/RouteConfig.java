30a81dfa2c313bfe61e285aff1d0c234aef0d8d4
serg <avdey1978@gmail.com>
2023-12-12 11:30:45 +0300

    Router config add

--- gateway/src/main/java/ru/avdeev/gateway/config/RouteConfig.java
+++ gateway/src/main/java/ru/avdeev/gateway/config/RouteConfig.java
@@ -1,29 +1,19 @@
 package ru.avdeev.gateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Configuration
 public class RouteConfig {
 
-    @Bean
-    public RouterFunction<ServerResponse> htmlRouter(@Value("classpath:/static/ttc-tops/index.html") Resource html) {
-        return route(GET("/ttc-tops/")
-                .or(GET("/"))
-                .or(GET("/ttc-tops"))
-                .or(GET("/ttc-tops/rent"))
-                .or(GET("/ttc-tops/training"))
-                .or(GET("/ttc-tops/tournament")), request
-                -> ok().contentType(MediaType.TEXT_HTML).syncBody(html)
-        );
-    }
+//    @Bean
+//    public RouterFunction<ServerResponse> htmlRouter(@Value("classpath:/static/ttc-tops/index.html") Resource html) {
+//        return route(GET("/ttc-tops/")
+//                .or(GET("/"))
+//                .or(GET("/ttc-tops"))
+//                .or(GET("/ttc-tops/rent"))
+//                .or(GET("/ttc-tops/training"))
+//                .or(GET("/ttc-tops/tournament")), request
+//                -> ok().contentType(MediaType.TEXT_HTML).syncBody(html)
+//        );
+//    }
 }
