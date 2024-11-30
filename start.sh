3035121937f97965a6a953ba48bd33d63b702553
sergeiavdeev <sergei_avdeev@mail.ru>
2024-11-30 12:37:36 +0300

    update

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+docker ps -aq --filter "name=config-server" | xargs -r docker rm -f
+docker ps -aq --filter "name=gateway" | xargs -r docker rm -f
+docker ps -aq --filter "name=eureka" | xargs -r docker rm -f
+docker pull sergeiavdeev/eureka:latest
+docker pull sergeiavdeev/gateway:latest
+docker pull sergeiavdeev/config-server:latest
 PROFILE=dev ENCRYPT_KEY=9edc7779-04e8-4275-ae43-ebedb8555b99 docker compose up -d
+docker system prune -a -f
